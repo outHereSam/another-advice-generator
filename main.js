10112c5b1e990b57671bcf3127412a44458f822b
Samuel Larbi Danquah <larbisamuel0@gmail.com>
2024-06-19 14:58:15 +0000

    Makes code more readable

--- main.js
+++ main.js
@@ -2,48 +2,74 @@ const adviceText = document.querySelector(".advice");
 const adviceId = document.querySelector(".advice-id");
 const diceBtn = document.getElementById("dice");
 
-// Load random advice when the page first loads
-// const fetchRandomAdvice = async () => {
-//   const response = await fetch("	https://api.adviceslip.com/advice");
-//   const data = await response.json();
-//   return data;
+// With Async/Await
+const fetchRandomAdvice = async () => {
+  const response = await fetch("	https://api.adviceslip.com/advice");
+  const data = await response.json();
+  return data;
+};
+
+// With Promise
+// const fetchRandomAdvice = () => {
+//   return new Promise((resolve, reject) => {
+//     fetch("	https://api.adviceslip.com/advice")
+//       .then((response) => {
+//         if (response) {
+//           return response.json();
+//         } else {
+//           throw new Error("Error! Could Not Fetch Data");
+//         }
+//       })
+//       .then((data) => resolve(data))
+//       .catch((err) => reject(err));
+//   });
 // };
 
-// Trying with Promise
-const fetchRandomAdvice = () => {
-  return new Promise((resolve, reject) => {
-    fetch("	https://api.adviceslip.com/advice")
-      .then((response) => {
-        if (response) {
-          return response.json();
-        } else {
-          throw new Error("Error! Could Not Fetch Data");
-        }
-      })
-      .then((data) => resolve(data))
-      .catch((err) => reject(err));
-  });
-};
+// With callbacks
+// function fetchRandomAdvice(successCallback, failureCallback) {
+//   fetch("	https://api.adviceslip.com/advice")
+//     .then((response) => {
+//       if (response) {
+//         return response.json();
+//       } else {
+//         throw new Error("Error! Could Not Fetch Data");
+//       }
+//     })
+//     .then((data) => successCallback(data))
+//     .catch((err) => failureCallback(err));
+// }
+
+// fetchRandomAdvice(
+//   (data) => {
+//     adviceText.textContent = data.slip.advice;
+//     adviceId.textContent = `Advice #${data.slip.id}`;
+//     console.log(data);
+//   },
+//   (error) => {
+//     console.log(error);
+//   }
+// );
 
 try {
   fetchRandomAdvice()
     .then((data) => {
-      adviceText.textContent = data.slip.advice;
-      adviceId.textContent = `Advice #${data.slip.id}`;
-      console.log(data);
+      setAdviceText(data);
     })
     .catch((err) => console.log(err));
 } catch (error) {
   throw new Error();
 }
 
+const setAdviceText = (data) => {
+  adviceText.textContent = `"${data.slip.advice}"`;
+  adviceId.textContent = `Advice #${data.slip.id}`;
+};
+
 diceBtn.addEventListener("click", () => {
   try {
     fetchRandomAdvice()
       .then((data) => {
-        adviceText.textContent = data.slip.advice;
-        adviceId.textContent = `Advice #${data.slip.id}`;
-        console.log(data);
+        setAdviceText(data);
       })
       .catch((err) => console.log(err));
   } catch (error) {
